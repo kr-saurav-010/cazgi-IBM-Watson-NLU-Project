93b2f293b1933ca5aca1f9f9e5111039ab99f7a1
Kumar Saurav <kr.saurav.010@gmail.com>
2021-07-18 09:47:45 +0000

    added getNLUInstance

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = new express();
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance(){
+    let api_key = process.env.api_key
+    let api_url = process.env.api_url
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-03-25',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding
+}
+
+
 app.use(express.static('client'))
 
 const cors_app = require('cors');
